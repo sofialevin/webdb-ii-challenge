04d2e91c883cd700a2fb6a3b20bd7cd6f9ac4431
Sofia Levin <sofia@Joels-MacBook-Pro.local>
2019-12-10 14:23:36 -0800

    Set up migration file and ran first migration.

---
+++ migrations/20191210142303_create-cars.js
@@ -0,0 +1,15 @@
+exports.up = function(knex) {
+  return knex.schema.createTable('cars', tbl => {
+    tbl.increments();
+    tbl.text('VIN', 128).unique().notNullable();
+    tbl.text('make', 128).notNullable();
+    tbl.text('model', 128).notNullable();
+    tbl.decimal('mileage', 128).notNullable();
+    tbl.text('transmission_type', 128)
+    tbl.text('status_of_title', 128)
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('cars');
+};
