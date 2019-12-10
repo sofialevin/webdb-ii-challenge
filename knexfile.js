d8c6d4b92937faa4010fdf53fedafd3a1e946244
Sofia Levin <sofia@Joels-MacBook-Pro.local>
2019-12-10 14:39:33 -0800

    Completed CREATE and READ operations.

--- knexfile.js
+++ knexfile.js
@@ -5,9 +5,9 @@ module.exports = {
     connection: {
       filename: './data/car-dealer.db3'
     },
+    useNullAsDefault: true,
     migrations: {
       directory: './data/migrations'
     },
-    useNullAsDefault: true
   }
 };
--- server.js
+++ server.js
@@ -6,4 +6,41 @@ const server = express();
 
 server.use(express.json());
 
+server.get('/', (req, res) => {
+  db('cars')
+    .then(cars => {
+      res.json(cars);
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'Failed to retrieve cars', error: err });
+    });
+});
+
+server.get('/:id', (req, res) => {
+  const { id } = req.params;
+
+  db('cars').where({ id }).first()
+    .then(car => {
+      res.json(car);
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'Failed to retrieve car', error: err });
+    });
+});
+
+server.post('/', (req, res) => {
+  const carData = req.body;
+  db('cars').insert(carData)
+    .then(ids => {
+      db('cars').where({ id: ids[0] })
+        .then(newCarEntry => {
+          res.status(201).json(newCarEntry);
+        });
+    })
+    .catch(err => {
+      console.log('POST error', err);
+      res.status(500).json({ message: "Failed to store data", error: err });
+    });
+});
+
 module.exports = server;
