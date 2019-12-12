81b7c872597944302d007eff2d0261fb71bf17e8
Sofia Levin <sofia@Joels-MacBook-Pro.local>
2019-12-11 21:20:05 -0800

    Added index to make and model.

--- data/migrations/20191210142303_create-cars.js
+++ data/migrations/20191210142303_create-cars.js
@@ -2,8 +2,8 @@ exports.up = function(knex) {
   return knex.schema.createTable('cars', tbl => {
     tbl.increments();
     tbl.text('VIN', 128).unique().notNullable();
-    tbl.text('make', 128).notNullable();
-    tbl.text('model', 128).notNullable();
+    tbl.text('make', 128).index().notNullable();
+    tbl.text('model', 128).index().notNullable();
     tbl.decimal('mileage', 128).notNullable();
     tbl.text('transmission_type', 128)
     tbl.text('status_of_title', 128)
